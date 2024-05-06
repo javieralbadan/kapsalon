96f11e76dc1ca5eebe09545536feb11562aed180
Javier Albadan <javier.albadan@gmail.com>
2024-05-06 18:21:35 +0200

    feat: Add base app config

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import React from "react";
+import "../styles/globals.scss";
+
+export const metadata: Metadata = {
+  title: "Kapsalon · Agenda tu cita de barbería en pocos minutos",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ middleware.ts
@@ -0,0 +1,88 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access,  @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument */
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+        remove(name: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+        },
+      },
+    }
+  );
+
+  const { data } = await supabase.auth.getUser();
+  const redirectUrl = request.nextUrl.clone();
+  const { pathname } = redirectUrl;
+
+  // If the user is logged in and tries to access the home page, redirect to the dashboard
+  if (data.user && pathname === "/") {
+    redirectUrl.pathname = "/dashboard";
+    return NextResponse.redirect(redirectUrl);
+  }
+  // If the user is not logged in and tries to access a page other than the home page or the callback page, redirect to the home page
+  if (!data.user && pathname !== "/" && pathname !== "/auth/callback") {
+    redirectUrl.pathname = "/";
+    redirectUrl.searchParams.set(`next`, pathname);
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  return response;
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
